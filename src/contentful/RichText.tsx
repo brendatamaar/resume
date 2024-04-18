37d2d22ea3fbdf3c7d750392898e3b890ec19fe1
brendatamaar <brendatama10@gmail.com>
2024-04-18 16:48:44 +0700

    redesign blog post

--- src/contentful/RichText.tsx
+++ src/contentful/RichText.tsx
@@ -13,20 +13,76 @@ const renderOptions: Options = {
 		[BLOCKS.UL_LIST]: (node: any, children: React.ReactNode) => <ul className="ml-3 list-disc">{children}</ul>,
 		[BLOCKS.LIST_ITEM]: (node: any, children: React.ReactNode) => <li className="my-2 [&>p]:my-0">{children}</li>,
 		[BLOCKS.QUOTE]: (node: any, children: React.ReactNode) => <blockquote className="my-8 p-4 border-s-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800">{children}</blockquote>,
+		[BLOCKS.HEADING_1]: (node: any, children: React.ReactNode) => <h1 className="text-black dark:text-white text-medium font-medium my-4">{children}</h1>,
+		[BLOCKS.HEADING_2]: (node: any, children: React.ReactNode) => <h2 className="text-black dark:text-white text-medium font-medium my-4">{children}</h2>,
+		[BLOCKS.HEADING_3]: (node: any, children: React.ReactNode) => <h3 className="text-black dark:text-white text-medium font-medium my-4">{children}</h3>,
+		[BLOCKS.HEADING_4]: (node: any, children: React.ReactNode) => <h4 className="text-black dark:text-white text-medium font-medium my-4">{children}</h4>,
+		[BLOCKS.HEADING_5]: (node: any, children: React.ReactNode) => <h5 className="text-black dark:text-white text-medium font-medium my-4">{children}</h5>,
+		[BLOCKS.HEADING_6]: (node: any, children: React.ReactNode) => <h6 className="text-black dark:text-white text-medium font-medium my-4">{children}</h6>,
 		[INLINES.HYPERLINK]: (node, children) => (
-            <a href={node.data.uri} className="font-medium underline hover:no-underline text-blue-600 dark:text-blue-500" target='_blank'>
-                {children}
-            </a>
-        ),
+			<a href={node.data.uri} className="font-medium underline hover:no-underline text-blue-600 dark:text-blue-500" target='_blank'>
+				{children}
+			</a>
+		),
+
+		[INLINES.EMBEDDED_ENTRY]: (node, children) => {
+			return (
+				<a href={`/blog/${node.data.target.fields.slug}`}>
+					{node.data.target.fields.title}
+				</a>
+			);
+		},
+		[BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
+
+			if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
+				return (
+					<iframe
+						src={node.data.target.fields.embedUrl}
+						height="100%"
+						width="100%"
+						frameBorder="0"
+						scrolling="no"
+						title={node.data.target.fields.title}
+						allowFullScreen={true}
+					/>
+				);
+			}
+
+		},
+
+		[BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+			return (
+				<figure>
+					<img
+						className="h-auto mx-auto max-w-full rounded-lg"
+						src={`https://${node.data.target.fields.file.url}`}
+						height={node.data.target.fields.file.details.image.height}
+						width={node.data.target.fields.file.details.image.width}
+						alt={node.data.target.fields.title} />
+					<figcaption className="mt-2 font-medium text-medium text-center text-gray-500 dark:text-gray-400">{node.data.target.fields.description}</figcaption>
+				</figure>
+			);
+		},
 	},
 	renderMark: {
 		[MARKS.BOLD]: (text) => <strong className='text-medium font-medium text-black dark:text-white'>{text}</strong>,
 		[MARKS.ITALIC]: (text) => <em>{text}</em>,
 		[MARKS.UNDERLINE]: (text) => <u>{text}</u>,
 		[MARKS.CODE]: (text) =>
-			<code className="bg-gray-100 dark:bg-gray-800 p-4 rounded-lg overflow-x-auto my-4">
-				{text}
-			</code>
+			<div className="relative max-w-2xl mx-auto">
+				<div className="bg-gray-900 text-white p-4 rounded-md">
+					<div className="flex justify-between items-center mb-6">
+						<span className="text-gray-400">Code:</span>
+					</div>
+					<div className="overflow-x-auto pb-6">
+						<pre >
+							<code id='code' className="text-gray-300">
+								{text}
+							</code>
+						</pre>
+					</div>
+				</div>
+			</div>
 	},
 	preserveWhitespace: true
 };
